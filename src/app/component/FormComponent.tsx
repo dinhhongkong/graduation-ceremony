31ed8b18a492477ff2a72fc1868ee37d9d167ea7
dinhhongkong <dinhhongkong2002@gmail.com>
2025-06-24 08:49:02 +0700

    fix ui form

--- src/app/component/FormComponent.tsx
+++ src/app/component/FormComponent.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const FormComponent: React.FC = () => {
   return (
-    <div className="mx-auto p-4 bg-gray-100">
+    <div className="mx-auto p-4 pt-18 bg-gray-100">
       <h2 className="text-3xl md:text-4xl text-[#0D3B66] font-bold text-center">
         Nhắn Gửi Yêu Thương
       </h2>
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSeL7cHr-N24dFF_AmMpGjzfR-mL0PxYBzDVmt4-odIGWeWogw/viewform?embedded=true"
         width="100%"
-        height="1600"
+        height="1000"
         className="border-none w-full"
         title="Google Form"
       >
